59309999171f92e41e464a3a4cf46a1b7f8f9f08
Ai1023 <126910091+Ai1023@users.noreply.github.com>
2023-04-06 10:36:52 +0000

    [Add]tableタグ

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,8 @@
 class SearchesController < ApplicationController
-  
+
   def search
     @range = params[:range]
+    @word = params[:word]
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
     else
